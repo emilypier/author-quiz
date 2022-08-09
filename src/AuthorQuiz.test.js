a07ca2be0b59c3d39fcfadabfbce0b66cf56e20d
emilypier <piersoee@miamioh.edu>
2022-08-09 13:05:53 -0400

    quiz functionality

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthorQuiz from './AuthorQuiz';
 
+describe("Author Quiz", ()=>{
+  it("Renders without crashing", ()=>{
+    const div=document.createElement("div");
+    ReactDOM.render(<AuthorQuiz/>, div);
+  })
+})
+/*
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
--- src/index.js
+++ src/index.js
@@ -1,17 +1,123 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './index.css';
+import AddAuthorForm from './AddAuthorForm'
 import AuthorQuiz from './AuthorQuiz';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl:'images/authors/marktwain.jpg',
+    imageSource:'Wikimedia Commons',
+    books:['The Adventures of Huckleberry Finn', 'The Adventures of Tom Sawyer' ]
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl:'images/authors/josephconrad.jpg',
+    imageSource:'Wikimedia Commons',
+    books:['Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl:'images/authors/jkrowling.jpg',
+    imageSource:'Wikimedia Commons',
+    imageAttribution:'Daniel Ogren',
+    books:['Harry Potter and the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl:'images/authors/stephenking.jpg',
+    imageSource:'Wikimedia Commons',
+    imageAttribution:'Pinguino',
+    books:['The Shining','IT']
+  }, 
+  {
+    name:'Charles Dickens',
+    imageUrl:'images/authors/charlesdickens.jpg',
+    imageSource:'Wikimedia Commons',
+    books:['David Copperfield', 'A Tale of Two City']
+  },
+  {
+    name:'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource:'Wikimedia Commons',
+    books:['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  },
+  {
+    name:'Toni Morrison',
+    imageUrl: 'images/authors/tonimorrison.jpg',
+    imageSource:'Wikimedia Commons',
+    books:['Song of Solomon', 'Beloved', 'The Bluest Eye']
+  },
+];
+
+//choosing the author that has written a book with the 
+//same name as the answer user selected.
+function getTurnData(authors){
+  const allBooks= authors.reduce(function (p,c,i){
+    return p.concat(c.books);
+  },[]);
+  //using underscore library. takes first 4 elements from list
+  const fourRandomBooks=shuffle(allBooks).slice(0,4);
+  const answer = sample(fourRandomBooks);
+
+  // find an author such taht the author's books collection  
+  // contains a book where the title is = answer user chose.
+  return{
+    books:fourRandomBooks,
+    author: authors.find((author)=>
+    author.books.some((title)=>
+        title===answer))
+  }
+}
+
+function reducer( state={authors, turnData:getTurnData(authors), highlight:''}, action) {
+    switch (action.type){
+    case 'ANSWER_SELECTED':
+    const isCorrect=state.turnData.author.books.some((book)=>book===action.answer);
+    return Object.assign(
+      {},
+      state,{
+        highlight: isCorrect ? 'correct' : 'wrong'
+    });
+    case 'CONTINUE':
+    return Object.assign({},state,{
+      highlight:'',
+      turnData:getTurnData(state.authors)
+    });
+    case 'ADD_AUTHOR':
+    return Object.assign({},state,{
+      authors:state.authors.concat([action.author])
+    }) 
+    default: return state;
+  }
+}
+
+let store =Redux.createStore(reducer);
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <AuthorQuiz />
-  </React.StrictMode>
+    <BrowserRouter>
+    <ReactRedux.Provider store={store}>
+      <React.Fragment>
+        <Route exact path="/" component={AuthorQuiz}/>
+        <Route path="/add" component={AddAuthorForm}/>
+      </React.Fragment>
+      </ReactRedux.Provider>
+    </BrowserRouter>  
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
