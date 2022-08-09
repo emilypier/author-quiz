b64b8fdc8a5ab4de6a89ffa170c8d1137a8a2205
emilypier <piersoee@miamioh.edu>
2022-08-09 10:14:44 -0400

    set up AuthorQuiz component

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css'
+
+function AuthorQuiz() {
+  return (
+    <div>Author Quiz</div>
+  );
+}
+
+export default AuthorQuiz;
